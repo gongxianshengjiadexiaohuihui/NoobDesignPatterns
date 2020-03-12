256242ce3ba50af278e07b5727dac1cdc60cf944
gongxianshengjiadexiaohuihui <937827303@qq.com>
2020-03-12 21:55:35 +0800

    策略模式+lambda表达式

--- src/main/java/com/ggp/design_patterns/stategy/Main.java
+++ src/main/java/com/ggp/design_patterns/stategy/Main.java
@@ -12,8 +12,21 @@ public class Main {
         Cat[] cats={new Cat(5,1),new Cat(4,2),new Cat(3,3)};
         Sorter<Cat> sorter = new Sorter<>();
         Cat[] a = sorter.sort(cats,new CompareWeightComparator());
+
         System.out.println(Arrays.toString(a));
-        a = sorter.sort(cats,new CompareHeightComparator());
+       a = sorter.sort(cats,new CompareHeightComparator());
+        /**
+         * lambda表达式
+         */
+//        a = sorter.sort(cats,(t1,t2)->{
+//            if(t1.height>t2.height){
+//                return 1;
+//            }else if(t1.height<t2.height){
+//                return -1;
+//            }else {
+//                return 0;
+//            }
+//        });
         System.out.println(Arrays.toString(a));
     }
 }
